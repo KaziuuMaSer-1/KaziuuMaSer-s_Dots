18b35c23f13b1dcf0ed952318396d6cfc9e4d4f8
KaziuuMaSer-1 <202332518+KaziuuMaSer-1@users.noreply.github.com>
2026-06-28 14:48:29 +0200

    Sum changes in fish abbrs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,5 @@
+abbr --add ocr "bash ~/Scripts/scan-pdf.sh"
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+abbr --add update "sudo apt update -y && sudo apt upgrade -y && flatpak update -y && brew update"
 abbr --add gac "git add . && git commit -m"
+abbr --add mal "cd ~/Git/mal-tui; and ./target/release/mal-tui"
